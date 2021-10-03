070de1398af7c86f8a1386fbe8e9392aa42159db
Alexandre Plateau <lexplt.dev@gmail.com>
2021-10-03 18:10:39 +0200

    enhancing rm to handle -d -di -dI -dir -dIr -rf -r

--- rm.py
+++ rm.py
@@ -1,11 +1,48 @@
 #!/usr/bin/env python3
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 import os
 import sys
 import argparse
+import shutil
 import utils
+
+
+def remove(path: str, recursive: bool = False):
+    try:
+        if recursive:
+            if not os.path.isdir(path):
+                utils.log_error("rm", f"failed to remove '{path}': Not a directory")
+                return 1
+            shutil.rmtree(path)
+        else:
+            os.remove(path)
+    except FileNotFoundError:
+        utils.log_error("rm", f"failed to remove '{path}': No such file or directory")
+        return 1
+    except OSError:
+        utils.log_error("rm", f"failed to remove '{path}': Directory not empty")
+        return 1
+    else:
+        return 0
+
+
+def rmdir(path: str):
+    if not os.path.isdir(path):
+        utils.log_error("rm", f"failed to remove '{path}': Not a directory")
+        return 1
+
+    try:
+        os.rmdir(path)
+    except FileNotFoundError:
+        utils.log_error("rm", f"failed to remove '{path}': No such file or directory")
+        return 1
+    except OSError:
+        utils.log_error("rm", f"failed to remove '{path}': Directory not empty")
+        return 1
+    else:
+        return 0
 
 
 def main():
@@ -62,27 +99,64 @@ def main():
     )
 
     args = parser.parse_args()
-    print(args)
 
     if args.version:
         print(f"rm (Python alias mimicking Bash rm) {__version__}")
         return 0
 
+    code = 0
+
     if args.dir:
+        if args.I:
+            # interactive one time
+            count = len(args.files)
+            recur = " recursively" if args.recursive else ""
+            question = input(
+                f"rm: remove {count} argument{utils.plural(count)}{recur}?"
+            )
+
+            if question.lower() not in utils.VALID:
+                code = 1
+            else:
+                for file in args.files:
+                    errcode = (
+                        remove(file, args.recursive) if args.recursive else rmdir(file)
+                    )
+                    if errcode != 0:
+                        code = errcode
+        else:
+
+            def work(files, interactive: bool = False, recursive: bool = False):
+                nonlocal code
+
+                for file in files:
+                    if interactive and not recursive:
+                        question = input(f"rm: remove directory '{file}'? ")
+                        if question.lower() not in utils.VALID:
+                            continue
+
+                    if interactive:
+                        question = input(f"rm: descend into directory '{file}'? ")
+                        if question.lower() in utils.VALID:
+                            work(list(os.scandir(file)), interactive, recursive)
+                    else:
+                        errcode = rmdir(file)
+                    if errcode != 0:
+                        code = errcode
+
+            work(args.files, args.i, args.recursive)
+    elif args.recursive and not args.I and not args.i:
         for file in args.files:
-            try:
-                os.rmdir(file)
-            except FileNotFoundError:
-                utils.log_error("rm", f"failed to remove '{file}': No such file or directory")
-            except OSError:
-                utils.log_error("rm", f"failed to remove '{file}: Directory not empty")
+            errcode = remove(file, True)
+            if errcode != 0:
+                code = errcode
+                if not args.force:
+                    break
+    else:
+        utils.log_error("rm", f"unhandled option combination: {args}")
+        code = -1
 
-            if not os.path.isdir(file):
-                utils.log_error("rm", f"failed to remove '{file}': Not a directory")
-                return -1
-        return 0
-
-    return 0
+    return code
 
 
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -2,6 +2,12 @@
 
 __version__ = "0.0.1"
 
+VALID = ("y", "yes")
+
 
 def log_error(command: str, msg: str):
     print(f"{command}: {msg}")
+
+
+def plural(count: int):
+    return "s" if count > 1 else ""
